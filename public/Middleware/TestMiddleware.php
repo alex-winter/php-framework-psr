aa1a94d3ce03519d21fe3af2dc445e30aa00f8d4
Alex Winter <ajwinter42@gmail.com>
2025-04-10 15:03:55 +0100

    a

--- public/Middleware/TestMiddleware.php
+++ public/Middleware/TestMiddleware.php
@@ -11,8 +11,6 @@ class TestMiddleware implements MiddlewareInterface
 {
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        echo "lll";
-
         return $handler->handle($request);
     }
 }
