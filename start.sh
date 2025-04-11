7d2bbf123ca9aa0d05a4d3c94ea202819aa2950b
Alex Winter <ajwinter42@gmail.com>
2025-04-11 19:06:20 +0100

    add migrations to start sh

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
 docker-compose down -v
-docker network prune
+docker network prune -f
 docker-compose up -d
+
+docker-compose run --rm dependencies
+
+docker-compose exec app ./vendor/bin/doctrine-migrations migrate --no-interaction
